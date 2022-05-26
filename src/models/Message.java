ecaec2d0853ecf6de27f777228715b3321c80e06
yuna.kawamura <YUNA.KAWAMURA@grp.tmnf.jp>
2022-05-26 14:38:51 +0900

    ページネーションを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -15,10 +15,15 @@ import javax.persistence.Table;
 @Entity
 @NamedQueries({
     @NamedQuery(
-        name = "getAllMessages",
-        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
-    )
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            ),
+    @NamedQuery(
+            name = "getMessagesCount",
+            query = "SELECT COUNT(m) FROM Message AS m"
+            )
 })
+
 @Table(name = "messages")
 public class Message {
     @Id
